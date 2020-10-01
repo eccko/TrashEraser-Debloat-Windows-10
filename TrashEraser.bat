77431bbbf9d296f4d920c0001033d108ca653e1a
Matthew <41365572+eccko@users.noreply.github.com>
2020-10-01 17:56:25 +0200

    powershell regrets

--- TrashEraser.bat
+++ TrashEraser.bat
@@ -1,8 +1,8 @@
 @echo off
 
-title: Writted by eccko
+title: Compilation 01.10.2020
 
-echo github.com/eccko
+echo Writed by Mateusz Tomczyk - eccko
 
  rem  set __COMPAT_LAYER=RunAsInvoker  
  REGEDIT.EXE  /S  "%~dp0\reg_files\game_priority.reg"
@@ -11,14 +11,14 @@ echo github.com/eccko
  REGEDIT.EXE  /S  "%~dp0\reg_files\disable_onedrive.reg"
 
  rem  set __COMPAT_LAYER=RunAsInvoker  
- REGEDIT.EXE  /S  "%~dp0\reg_files\unnecessary_staff.reg"
- 
- rem  set __COMPAT_LAYER=RunAsInvoker  
- REGEDIT.EXE  /S  "%~dp0\reg_files\cortana.reg"
+ REGEDIT.EXE  /S  "%~dp0\reg_files\unnecessary_staff.reg
 
- rem  set __COMPAT_LAYER=RunAsInvoker  
- REGEDIT.EXE  /S  "%~dp0\reg_files\photo_viewer.reg"
 
 Del /S /F /Q %temp%
+
+
+powershell -noprofile -command "&{ start-process powershell -ArgumentList '-noprofile -executionpolicy remotesigned -file %~dp0\ps_scripts\stock_apps.ps1' -verb RunAs}"
+
+powershell -noprofile -command "&{ start-process powershell -ArgumentList '-noprofile -executionpolicy remotesigned -file %~dp0\ps_scripts\xbox.ps1' -verb RunAs}"
 
 pause
