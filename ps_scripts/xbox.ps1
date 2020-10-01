4af03513b800aa539fbbe8d8ad455975435c0242
Matthew <41365572+eccko@users.noreply.github.com>
2020-10-01 17:56:08 +0200

    +debloat xbox

---
+++ ps_scripts/xbox.ps1
@@ -0,0 +1,6 @@
+﻿Get-AppxPackage -AllUsers Microsoft.XboxGamingOverlay | Remove-AppxPackage
+Get-AppxPackage -AllUsers Microsoft.Xbox.TCUI | Remove-AppxPackage
+Get-AppxPackage -AllUsers Microsoft.XboxSpeechToTextOverlay | Remove-AppxPackage
+Get-AppxPackage -AllUsers Microsoft.XboxGameCallableUI | Remove-AppxPackage
+Get-AppxPackage -AllUsers Microsoft.XboxGameOverlay | Remove-AppxPackage
+Get-AppxPackage -AllUsers Microsoft.XboxIdentityProvider | Remove-AppxPackage
