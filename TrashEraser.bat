d0d274a8cef823f6cf35b878343922f237aa4074
Matthew <41365572+eccko@users.noreply.github.com>
2020-10-01 16:07:14 +0200

    Update TrashEraser.bat
    + added %temp%
    + reg files

--- TrashEraser.bat
+++ TrashEraser.bat
@@ -7,4 +7,13 @@ echo Writed by Mateusz Tomczyk - eccko
  rem  set __COMPAT_LAYER=RunAsInvoker  
  REGEDIT.EXE  /S  "%~dp0\reg_files\game_priority.reg"
 
+ rem  set __COMPAT_LAYER=RunAsInvoker  
+ REGEDIT.EXE  /S  "%~dp0\reg_files\disable_onedrive.reg"
+
+ rem  set __COMPAT_LAYER=RunAsInvoker  
+ REGEDIT.EXE  /S  "%~dp0\reg_files\unnecessary_staff.reg
+
+
+Del /S /F /Q %temp%
+
 pause
