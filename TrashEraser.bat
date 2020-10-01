7e8813a4fa3c397c4ff61e38c3c0b020a83866ac
Matthew <41365572+eccko@users.noreply.github.com>
2020-10-01 18:23:39 +0200

    auto admin

--- TrashEraser.bat
+++ TrashEraser.bat
@@ -1,6 +1,31 @@
 @echo off
-
 title: by eccko
+
+
+:: BatchGotAdmin
+:-------------------------------------
+REM  --> Check for permissions
+>nul 2>&1 "%SYSTEMROOT%\system32\cacls.exe" "%SYSTEMROOT%\system32\config\system"
+
+REM --> If error flag set, we do not have admin.
+if '%errorlevel%' NEQ '0' (
+    echo Request administrator permission...
+    goto UACPrompt
+) else ( goto gotAdmin )
+
+:UACPrompt
+    echo Set UAC = CreateObject^("Shell.Application"^) > "%temp%\getadmin.vbs"
+    set params = %*:"=""
+    echo UAC.ShellExecute "cmd.exe", "/c %~s0 %params%", "", "runas", 1 >> "%temp%\getadmin.vbs"
+
+    "%temp%\getadmin.vbs"
+    del "%temp%\getadmin.vbs"
+    exit /B
+
+:gotAdmin
+    pushd "%CD%"
+    CD /D "%~dp0"
+:--------------------------------------
 
 
  rem  set __COMPAT_LAYER=RunAsInvoker  
@@ -10,16 +35,8 @@ title: by eccko
  REGEDIT.EXE  /S  "%~dp0\reg_files\disable_onedrive.reg"
 
  rem  set __COMPAT_LAYER=RunAsInvoker  
- REGEDIT.EXE  /S  "%~dp0\reg_files\unnecessary_staff.reg"
+ REGEDIT.EXE  /S  "%~dp0\reg_files\unnecessary_staff.reg
 
- rem  set __COMPAT_LAYER=RunAsInvoker  
- REGEDIT.EXE  /S  "%~dp0\reg_files\cortana.reg"
-
- rem  set __COMPAT_LAYER=RunAsInvoker  
- REGEDIT.EXE  /S  "%~dp0\reg_files\photo_viewer.reg"
- 
- rem  set __COMPAT_LAYER=RunAsInvoker  
- REGEDIT.EXE  /S  "%~dp0\reg_files\defender.reg"
 
 Del /S /F /Q %temp%
 
@@ -28,4 +45,8 @@ powershell -noprofile -command "&{ start-process powershell -ArgumentList '-nopr
 
 powershell -noprofile -command "&{ start-process powershell -ArgumentList '-noprofile -executionpolicy remotesigned -file %~dp0\ps_scripts\xbox.ps1' -verb RunAs}"
 
+cls
+echo TrashEraser executed successfully
+echo *****
+echo *****
 pause
