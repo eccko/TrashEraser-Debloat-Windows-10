e9cc70efb225da23f77a5e5d844845fc1a6d096f
Matthew <41365572+eccko@users.noreply.github.com>
2020-10-01 15:43:56 +0200

    Add files via upload

---
+++ TrashEraser.bat
@@ -0,0 +1,10 @@
+@echo off
+
+title: Compilation 01.10.2020
+
+echo Writed by Mateusz Tomczyk - eccko
+
+ rem  set __COMPAT_LAYER=RunAsInvoker  
+ REGEDIT.EXE  /S  "%~dp0\reg_files\game_priority.reg"
+
+pause
