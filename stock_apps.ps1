bc162a6837b0b48056258ebabc6c75b374e6d7a3
Matthew <41365572+eccko@users.noreply.github.com>
2020-10-01 17:54:19 +0200

    Add files via upload

---
+++ stock_apps.ps1
@@ -0,0 +1,99 @@
+
+get-appxpackage -allusers *3dbuilder* | remove-appxpackage
+
+get-appxpackage -allusers *alarms* | remove-appxpackage
+
+get-appxpackage -allusers *appconnector* | remove-appxpackage
+
+get-appxpackage -allusers *communicationsapps* | remove-appxpackage
+
+get-appxpackage -allusers *calculator* | remove-appxpackage
+
+get-appxpackage -allusers *camera* | remove-appxpackage
+
+
+get-appxpackage -allusers *feedback* | remove-appxpackage
+
+get-appxpackage -allusers *officehub* | remove-appxpackage
+
+get-appxpackage -allusers *getstarted* | remove-appxpackage
+
+
+get-appxpackage -allusers *skypeapp* | remove-appxpackage
+
+
+get-appxpackage -allusers *zunemusic* | remove-appxpackage
+
+
+get-appxpackage -allusers *zune* | remove-appxpackage
+
+
+get-appxpackage -allusers *maps* | remove-appxpackage
+
+get-appxpackage -allusers *messaging* | remove-appxpackage
+
+
+get-appxpackage -allusers *solitaire* | remove-appxpackage
+
+
+get-appxpackage -allusers *wallet* | remove-appxpackage
+
+
+get-appxpackage -allusers *connectivitystore* | remove-appxpackage
+
+get-appxpackage -allusers *bingfinance* | remove-appxpackage
+
+
+get-appxpackage -allusers *bing* | remove-appxpackage
+
+get-appxpackage -allusers *zunevideo* | remove-appxpackage
+
+
+get-appxpackage -allusers *bingnews* | remove-appxpackage
+
+
+get-appxpackage -allusers *onenote* | remove-appxpackage
+
+
+get-appxpackage -allusers  *oneconnect* | remove-appxpackage
+
+
+get-appxpackage -allusers *mspaint* | remove-appxpackage
+
+
+get-appxpackage -allusers  *people* | remove-appxpackage
+
+
+get-appxpackage -allusers *commsphone* | remove-appxpackage
+
+get-appxpackage -allusers *windowsphone* | remove-appxpackage
+
+
+get-appxpackage -allusers *phone* | remove-appxpackage
+
+
+get-appxpackage -allusers *photos* | remove-appxpackage
+
+
+get-appxpackage -allusers *bingsports* | remove-appxpackage
+
+
+get-appxpackage -allusers *sticky* | remove-appxpackage
+
+
+get-appxpackage -allusers *sway* | remove-appxpackage
+
+
+get-appxpackage -allusers *3dViewer* | remove-appxpackage
+
+get-appxpackage -allusers *soundrecorder* | remove-appxpackage
+
+get-appxpackage -allusers *bingweather* | remove-appxpackage
+
+
+get-appxpackage -allusers *GetHelp* | remove-appxpackage
+
+
+get-appxpackage -allusers *holographic* | remove-appxpackage
+
+get-appxpackage -allusers *WindowsCamera* | remove-appxpackage
